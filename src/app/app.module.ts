3716540643fd2c0acfc11a5124f66d7b6c634f3d
zhangzd <zhangzd@kylinno.com>
2019-01-11 11:09:07 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChildComponent } from './components/child/child.component';
+import { FatherComponent } from './components/father/father.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    FatherComponent
   ],
   imports: [
     BrowserModule
