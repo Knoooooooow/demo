94017dee785f3e23dc3b8f1fcf8c9f1c8d295f7f
zhangzd <knoooooooow@outlook.com>
2019-01-27 18:44:01 +0800

    feat:详细信息循环假数据

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  private products: Array<Product>;
   constructor() { }
 
   ngOnInit() {
+    this.products = [
+      new Product(1,"第一个商品",2.99,2.5,"第一条假数据",["电子产品","硬件设备"]),
+      new Product(2,"第二个商品",2.99,3.5,"第二条假数据",["娱乐产品","硬件设备"]),
+      new Product(3,"第三个商品",2.99,4,"第三条假数据",["家具用品","硬件设备"]),
+      new Product(4,"第四个商品",2.99,3.5,"第四条假数据",["电子产品","硬件设备"]),
+      new Product(5,"第五个商品",2.99,2.5,"第五条假数据",["电子产品","硬件设备"]),
+      new Product(6,"第六个商品",2.99,5,"第六条假数据",["电子产品","硬件设备"]),
+    ]
   }
 
+}
+export class Product {
+  constructor(
+    public id:number,
+    public title:string,
+    public price:number,
+    public rating:number,
+    public desc:string,
+    public categories:Array<string>
+  ){
+
+  }
 }
