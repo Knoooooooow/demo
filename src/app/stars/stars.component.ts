fbbdd7ba3a0a40e035a71074b91cb67780a9d88e
zhangzd <knoooooooow@outlook.com>
2019-01-28 22:57:55 +0800

    feat:星星组件(more): 了解:  父→子传值，属性绑定，数据驱动

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stars.component.scss']
 })
 export class StarsComponent implements OnInit {
-
+  @Input() private rating: number=0;
+  private stars: Array<boolean>;
+  
   constructor() { }
 
   ngOnInit() {
+    this.getStar();
+  }
+  
+  getStar(){
+    this.stars= [];
+    for(let i =1;i<=5;i++)
+    {
+      this.stars.push(i > this.rating);
+    }
+    
   }
 
 }
