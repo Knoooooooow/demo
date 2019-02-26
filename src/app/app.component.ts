5019bd22d385b639f5ab6b1bb662de15313aea54
zhangzd <knoooooooow@outlook.com>
2019-02-26 22:04:01 +0800

    feat: 简易ngdocheck事件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,9 @@ export class AppComponent {
   priceQuoteHandle(e:PriceQuote){
     this.priceQuote = e;
   }
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    console.log(123);
+  }
 }
